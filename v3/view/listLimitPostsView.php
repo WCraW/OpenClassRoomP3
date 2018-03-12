573ebe30bc698c3c92dfeb623bc2f1a40ea9be37
WCraW <antoine.millot.sco@gmail.com>
2018-03-12 04:35:55 +0100

    ajout de dossier manquant !

---
+++ v3/view/listLimitPostsView.php
@@ -0,0 +1,23 @@
+<html>	
+<h1>COUCOU</h1>
+<h3></h3>
+<?php
+$PostManager = new PostManager(); // Création d'un objet
+$post = $PostManager->getLimitPosts();
+var_dump($PostManager);
+var_dump($post);
+var_dump($post->fetch());
+while ($donnees = $post->fetch())
+{
+?>
+    <p>
+    <?php echo $donnees['Id']; ?>
+    <?php echo $donnees['Titre']; ?>
+    <?php echo $donnees['Number']; ?>
+    <?php echo $donnees['Contenue']; ?>
+    <?php echo $donnees['Date']; ?>
+   </p>
+<?php
+}
+?>
+</html>
